fb331448c17eea68f8c504faa84bf19bab2645ea
FransvanEk <frans@fransvanek.nl>
2024-10-02 16:45:24 +0200

    edited the homepage text again

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello python World, now from the clouds!"}
+    return {"message": "Hello python World, now from the clouds from azure!"}
